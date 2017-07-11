41e237bca17117117fda0b7ff114647dd3ca7cdc
Jun Li <junl14@uci.edu>
2017-07-11 16:13:06 -0700

    Create quadratic.py

---
+++ quadratic.py
@@ -0,0 +1,11 @@
+import math
+def quadratic(a, b, c):
+	if [False for i in (a, b, c) if not isinstance(i, (int, float))]:
+		raise TypeError('invalid type')
+	q = b * b - 4 * a * c
+	if a == 0:
+		return print('a can\'t be 0')
+	elif q < 0:
+		return print('not valid root')
+	else:
+		return (-b + math.sqrt(q)) / (2 * a), (-b -math.sqrt(q)) / (2 * a), 
