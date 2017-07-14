afa147aa3df5fe64c3812d0fd78b7ee949f37fca
Jun Li <junl14@uci.edu>
2017-07-14 11:33:57 -0700

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -6,6 +6,6 @@ def quadratic(a, b, c):
 	if a == 0:
 		return print('a can\'t be 0')
 	elif q < 0:
-		return print('not valid root')
+		return print('don\'t have a valid root')
 	else:
 		return (-b + math.sqrt(q)) / (2 * a), (-b -math.sqrt(q)) / (2 * a), 
